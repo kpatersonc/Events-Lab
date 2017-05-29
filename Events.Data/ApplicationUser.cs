4984584580f5df1e60169e4a23be4e6d033de002
CORP\kpaterson <kpaterson@infusion.com>
2017-05-29 16:54:51 -0400

    Project Setup

--- Events.Data/ApplicationUser.cs
+++ Events.Data/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
@@ -11,6 +12,8 @@ namespace Events.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string FullName { get; set; } 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
---
+++ Events.Data/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Data
+{
+    public class Event
+    {
+        public Event()
+        {
+            this.IsPublic = true;
+            this.StartDateTime = DateTime.Now;
+        }
+        public TimeSpan? Duration { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public bool IsPublic { get; private set; }
+
+        public DateTime StartDateTime { get; private set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(200)]
+        public string Location { get; set; }
+     }
+}
